6f643112f0c9ddcbf24b058d08d0159d368a7ae8
Rajarethinam Deivendran <rajarethinam.rajarethinam@capgemini.com>
2021-05-01 17:37:18 +0530

    init

---
+++ src/com/prinspal/Prinspal.java
@@ -0,0 +1,17 @@
+package com.prinspal;
+
+public class Prinspal {
+
+	public static void main(String[] args) {
+		Prinspal prinspal = new Prinspal();
+		System.out.println(prinspal.getContact());
+	}
+	
+	private String getContact() {
+		return "9994958845";
+	}
+	
+	public String getCollageAddress() {
+		return "Dindigul@*(";
+	}
+}
---
+++ src/com/prinspal/Student.java
@@ -0,0 +1,9 @@
+package com.prinspal;
+
+public class Student {
+	public static void main(String[] args) {
+		Prinspal prinspal = new Prinspal();
+		System.out.println(prinspal.getCollageAddress());
+	}
+	
+}
