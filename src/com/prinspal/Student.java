e069fccd9c33b414169258d0460ab82f8d7baddb
Rajarethinam Deivendran <rajarethinam.rajarethinam@capgemini.com>
2021-05-01 17:38:36 +0530

    init

--- src/com/prinspal/Student.java
+++
@@ -1,9 +0,0 @@
-package com.prinspal;
-
-public class Student {
-	public static void main(String[] args) {
-		Prinspal prinspal = new Prinspal();
-		System.out.println(prinspal.getCollageAddress());
-	}
-	
-}
